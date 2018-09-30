d5ddf0a9df830f9d269385a3f70226eabf0f4789
Osspial <osspial@gmail.com>
2018-09-29 20:18:58 -0400

    Track value movement

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,81 @@
+use std::ptr::NonNull;
 use std::ops::{Deref, DerefMut};
 use std::cell::Cell;
 use std::marker::PhantomData;
+use std::fmt;
 
-#[derive(Debug)]
-pub struct MutTracker<T, K> {
-    value: T,
-    mutated: Cell<bool>,
+
+pub struct MoveMutSentinel<K> {
+    self_ptr: Cell<NonNull<MoveMutSentinel<K>>>,
     _key: PhantomData<K>
 }
 
-impl<T, K> MutTracker<T, K> {
+pub struct MoveMutTracker<T, K> {
+    value: T,
+    sentinel: MoveMutSentinel<K>
+}
+
+impl<T, K> MoveMutTracker<T, K> {
     #[inline(always)]
-    pub fn new(value: T) -> MutTracker<T, K> {
-        MutTracker {
+    pub fn new(value: T) -> MoveMutTracker<T, K> {
+        MoveMutTracker {
             value,
-            mutated: Cell::new(true),
+            sentinel: MoveMutSentinel::mutated()
+        }
+    }
+
+    #[inline(always)]
+    pub fn was_moved_or_mutated(this: &Self) -> bool {
+        this.sentinel.was_moved_or_mutated()
+    }
+
+    #[inline(always)]
+    pub fn set_unmutated(this: &Self, _key: K) {
+        this.sentinel.set_unmutated(_key);
+    }
+
+    #[inline(always)]
+    pub fn change_key<NK>(this: Self) -> MoveMutTracker<T, NK> {
+        MoveMutTracker {
+            value: this.value,
+            sentinel: this.sentinel.change_key()
+        }
+    }
+}
+
+impl<K> MoveMutSentinel<K> {
+    #[inline(always)]
+    pub fn mutated() -> MoveMutSentinel<K> {
+        MoveMutSentinel {
+            self_ptr: Cell::new(NonNull::dangling()),
             _key: PhantomData
         }
     }
 
     #[inline(always)]
-    pub fn was_mutated(this: &Self) -> bool {
-        this.mutated.get()
-    }
-
-    /// Reset the "was mutated" flag to `false`.
-    #[inline(always)]
-    pub fn reset_was_mutated(this: &Self, _key: K) {
-        this.mutated.set(false)
+    pub fn was_moved_or_mutated(&self) -> bool {
+        self.self_ptr.get().as_ptr() as *const MoveMutSentinel<K> != self as *const MoveMutSentinel<K>
     }
 
     #[inline(always)]
-    pub fn change_key<NK>(this: Self) -> MutTracker<T, NK> {
-        MutTracker {
-            value: this.value,
-            mutated: this.mutated,
+    pub fn set_mutated(&self) {
+        self.self_ptr.set(Self::mutated().self_ptr.get())
+    }
+
+    #[inline(always)]
+    pub fn set_unmutated(&self, _key: K) {
+        self.self_ptr.set(unsafe{ NonNull::new_unchecked(self as *const Self as *mut Self) })
+    }
+
+    pub fn change_key<NK>(self) -> MoveMutSentinel<NK> {
+        MoveMutSentinel {
+            self_ptr: Cell::new(self.self_ptr.get().cast()),
             _key: PhantomData
         }
     }
 }
 
-impl<T, K> Deref for MutTracker<T, K> {
+impl<T, K> Deref for MoveMutTracker<T, K> {
     type Target = T;
 
     #[inline(always)]
@@ -49,30 +84,78 @@ impl<T, K> Deref for MutTracker<T, K> {
     }
 }
 
-impl<T, K> DerefMut for MutTracker<T, K> {
+impl<T, K> DerefMut for MoveMutTracker<T, K> {
     #[inline(always)]
     fn deref_mut(&mut self) -> &mut T {
-        self.mutated.set(true);
+        self.sentinel.set_mutated();
         &mut self.value
     }
 }
 
-impl<T: Clone, K> Clone for MutTracker<T, K> {
+impl<T: Clone, K> Clone for MoveMutTracker<T, K> {
     #[inline(always)]
-    fn clone(&self) -> MutTracker<T, K> {
-        MutTracker::new(self.value.clone())
+    fn clone(&self) -> MoveMutTracker<T, K> {
+        MoveMutTracker::new(self.value.clone())
     }
 
     #[inline(always)]
     fn clone_from(&mut self, source: &Self) {
-        self.mutated.set(true);
+        self.sentinel.set_mutated();
         self.value.clone_from(source);
     }
 }
 
-impl<T, K> From<T> for MutTracker<T, K> {
+impl<T, K> From<T> for MoveMutTracker<T, K> {
     #[inline(always)]
-    fn from(t: T) -> MutTracker<T, K> {
-        MutTracker::new(t)
+    fn from(t: T) -> MoveMutTracker<T, K> {
+        MoveMutTracker::new(t)
+    }
+}
+
+impl<K> fmt::Debug for MoveMutSentinel<K> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.was_moved_or_mutated() {
+            true => write!(f, "MovedOrmutated"),
+            false => write!(f, "Unmutated")
+        }
+    }
+}
+
+impl<T: fmt::Debug, K> fmt::Debug for MoveMutTracker<T, K> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("MoveMutTracker")
+            .field("sentinel", &self.sentinel)
+            .field("value", &self.value)
+            .finish()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    struct Key;
+
+    #[test]
+    fn moved() {
+        let tracker: MoveMutTracker<_, Key> = MoveMutTracker::new(0);
+        assert_eq!(true, MoveMutTracker::was_moved_or_mutated(&tracker));
+
+        MoveMutTracker::set_unmutated(&tracker, Key);
+        assert_eq!(false, MoveMutTracker::was_moved_or_mutated(&tracker));
+
+        let tracker_on_heap = Box::new(tracker);
+        assert_eq!(true, MoveMutTracker::was_moved_or_mutated(&tracker_on_heap));
+    }
+
+    #[test]
+    fn mutated() {
+        let mut tracker: MoveMutTracker<_, Key> = MoveMutTracker::new(0);
+        assert_eq!(true, MoveMutTracker::was_moved_or_mutated(&tracker));
+
+        MoveMutTracker::set_unmutated(&tracker, Key);
+        assert_eq!(false, MoveMutTracker::was_moved_or_mutated(&tracker));
+
+        *tracker = 1;
+        assert_eq!(true, MoveMutTracker::was_moved_or_mutated(&tracker));
     }
 }
